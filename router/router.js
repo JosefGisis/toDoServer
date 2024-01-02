b32fd3f2df24666a8e9b071c617bb745914290b1
josef gisis <jsquared.gisis@gmail.com>
2024-01-02 18:55:44 -0500

    commit

--- router/router.js
+++ router/router.js
@@ -14,13 +14,24 @@ const users = require('./usersRouter.js')
 const lists = require('./listsRouter.js')
 const toDos = require('./toDosRouter.js')
 
+app.set('view engine', 'pug')
+
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
-if (app.get('env') === 'development') app.use(morgan('short'))
+app.use(morgan('short'))
 app.use('/api/users', users)
 app.use('/api/lists', lists)
 app.use('/api/to_dos', toDos)
+
+app.get('/', async (req, res) => {
+    try {
+        res.render('index', {title: 'hello there', message: 'hello there'})
+    } catch (err) {
+        console.error('error getting active list', err)
+        res.status(500).send('internal server error')
+    }
+})
 
 const port = 3000
 
