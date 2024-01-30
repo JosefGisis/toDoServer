d873689e5172cde7438f8ece8a96d51add729b72
josef gisis <jsquared.gisis@gmail.com>
2024-01-30 15:06:03 -0500

    removed seperate listtodos

--- src/api/1/router/toDos.router.js
+++ src/api/1/router/toDos.router.js
@@ -1,18 +1,21 @@
-const router = require('express').Router()
+const router = require('express').Router({ mergeParams: true })
 const controller = require('../controllers/toDos.controller')
 
+// returns all to-dos associated with active list
 router.get('/', controller.toDos)
 
 router.get('/:toDoId', controller.toDo)
 
+// posts to-do with membership in active list
 router.post('/', controller.postToDo)
 
 router.delete('/:toDoId', controller.deleteToDo)
 
+// deletes all to-dos within active list
 router.delete('/', controller.deleteToDos)
 
 router.put('/:toDoId', controller.putToDo)
 
 router.put('/:toDoId/toggle', controller.toggleToDo)
 
-module.exports = router
+module.exports = router 
