76149d6d7322e1f65d6f3426d2830e24afe662c4
josef gisis <jsquared.gisis@gmail.com>
2024-02-14 14:29:30 -0500

    post passport refactor

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const cors = require('cors')
 const morgan = require('morgan')
 const passport = require('./src/api/1/middleware/auth.js').passport
-// const auth = require('./src/api/1/middleware/auth')
 const v1 = require('./src/api/1/router')
 const port = 3000
 // const helmet = require('helmet')
@@ -13,18 +12,15 @@ const express = require('express')
 
 const app = express()
 app.use(passport.initialize())
-// auth.init(passport)
-
-// pre passport refactor finalization
-console.log(passport)
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan('short'))
 
-app.use('/api/1', v1(passport))
+app.use('/api/1', v1)
 
 // app.use(helmet())
+
 app.use((err, req, res, next) => {
     console.log(err)
     res.status(500).json({ message: err.message })
@@ -35,5 +31,3 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
-
-// module.exports.passport = passport
--- src/api/1/middleware/auth.js
+++ src/api/1/middleware/auth.js
@@ -3,27 +3,6 @@ const ExtractJWT = require('passport-jwt').ExtractJwt
 const database = require('../../../services/database')
 const passport = require('passport')
 
-// module.exports.init = function (passport) {
-// 	return passport.use(
-// 		new JWTstrategy(
-// 			{
-// 				secretOrKey: process.env.JWT_KEY,
-// 				jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-// 			},
-// 			async (jwt_payload, done) => {
-// 				try {
-// 					const userId = jwt_payload.id
-// 					const user = await database.usersDB.getUser({ userId })
-// 					if (!user) throw new Error('invalid user information')
-// 					done(null, user, jwt_payload)
-// 				} catch (error) {
-// 					done(error, null)
-// 				}
-// 			}
-// 		)
-// 	)
-// }
-
 passport.use(
 	new JWTstrategy(
 		{
@@ -42,10 +21,6 @@ passport.use(
 		}
 	)
 )
-
-// module.exports.authenticate = function (passport) {
-// 	return passport.authenticate('jwt', { session: false })
-// }
 
 module.exports.authenticate = function () {
 	return passport.authenticate('jwt', { session: false })
